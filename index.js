fd45e3ecbea16be9bff7a11e5b2acd1878cfbd8f
Daila Viteri <dailaviteri@gmail.com>
2018-03-22 18:03:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ return Object.assign({}, object, {[key]: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-Object [key] = value 
+object [key] = value 
   return object
 }
   
